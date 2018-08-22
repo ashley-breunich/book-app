94f6bf06546fdf8cb160800b7dcc2b626b5c1939
Matto <matt.mcquain@gmail.com>
2018-08-21 18:49:09 -0700

    first commit of random stuff

---
+++ server.js
@@ -0,0 +1,13 @@
+require ('dotenv').confit();
+
+const express = require('express');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/ping', (request, response) => {
+    response.send('pong');
+})
+
+app.listen(PORT, () => console.log('Listening on PORT', PORT));
